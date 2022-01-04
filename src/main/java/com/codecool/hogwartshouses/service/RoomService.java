af6789b3c1996da04d031e75baa4cbc0864361e9
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-04 16:12:31 +0100

    feat: create getAll method in RoomService class.

--- src/main/java/com/codecool/hogwartshouses/service/RoomService.java
+++ src/main/java/com/codecool/hogwartshouses/service/RoomService.java
@@ -1,13 +1,22 @@
 package com.codecool.hogwartshouses.service;
 
+import com.codecool.hogwartshouses.data_sample.RoomCreator;
+import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.service.DAO.RoomDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class RoomService {
 
     @Autowired
     private RoomDAO roomDAO;
+
+    public Set<Room> getAllRoom(){
+        return roomDAO.getAll();
+    }
+
 
 }
