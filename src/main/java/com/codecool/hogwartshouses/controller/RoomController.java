f99261572249ab583ffae47780c68789efabde66
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-04 16:54:47 +0100

    feat: add rooms/{roomId} endpoint to find special room.

--- src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,5 +36,12 @@ public class RoomController {
     public String createEmptyRoom(){
         roomServices.addNewRoom();
         return "redirect:";
+    }
+
+    @GetMapping(value = "rooms/{roomId}")
+    public String getRoomById(Model model, @PathVariable String roomId){
+        Room myRoom = roomServices.findRoomById(roomId);
+        model.addAttribute("myRoom", myRoom);
+        return "rooms";
     }
 }
