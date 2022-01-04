1109e52a61846ce12c7fb8768a61ee3274b01acf
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-04 16:09:55 +0100

    feat: create initialize method.

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -1,20 +1,32 @@
 package com.codecool.hogwartshouses.data_sample;
 
+import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.types.HouseType;
 import com.codecool.hogwartshouses.service.DAO.RoomMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.UUID;
+
 @Component
 public class RoomCreator {
 
-    @Autowired
-    RoomMemory roomMemory;
+   private final RoomMemory roomMemory;
 
     public void initialize() {
-    //TODO
+        Room room = new Room(UUID.randomUUID(), HouseType.GRYFFINDOR);
+        Room room1 = new Room(UUID.randomUUID(), HouseType.HUFFLEPUFF);
+        Room room2 = new Room(UUID.randomUUID(), HouseType.RAVENCLAW);
+        Room room3 = new Room(UUID.randomUUID(), HouseType.SLYTHERIN);
+        for (Room room4 : Arrays.asList(room, room1, room2, room3)) {
+            roomMemory.add(room4);
+        }
     }
 
-    public RoomCreator() {
+    @Autowired
+    public RoomCreator(RoomMemory roomMemory) {
+        this.roomMemory = roomMemory;
         initialize();
     }
 }
