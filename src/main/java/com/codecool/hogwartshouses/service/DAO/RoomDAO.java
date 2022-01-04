a6a37f887c5851e8ffb2de902da483f883dd82a4
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-04 16:10:46 +0100

    feat: add add and getAll methods to RoomDao interface.

--- src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
+++ src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
@@ -1,5 +1,12 @@
 package com.codecool.hogwartshouses.service.DAO;
 
+import com.codecool.hogwartshouses.model.Room;
+
+import java.util.Set;
+
 public interface RoomDAO {
+
+    void add(Room room);
+    Set<Room> getAll();
 
 }
