a37d850f9e8ace9fd475aee931f815466d87763b
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-04 16:54:19 +0100

    feat: cancel unused import statement.

--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -4,7 +4,6 @@ import com.codecool.hogwartshouses.model.types.HouseType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
