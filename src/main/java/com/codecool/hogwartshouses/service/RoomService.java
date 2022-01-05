9b7705b2f82c3d4f89803f573e520c537757a58f
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-05 11:11:05 +0100

    feat: implement addStudentToEmptyRoom method in RoomService.

--- src/main/java/com/codecool/hogwartshouses/service/RoomService.java
+++ src/main/java/com/codecool/hogwartshouses/service/RoomService.java
@@ -1,11 +1,14 @@
 package com.codecool.hogwartshouses.service;
 
 import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
 import com.codecool.hogwartshouses.model.types.HouseType;
 import com.codecool.hogwartshouses.service.DAO.RoomDAO;
+import com.codecool.hogwartshouses.service.DAO.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
@@ -16,6 +19,7 @@ public class RoomService {
 
     @Autowired
     private RoomDAO roomDAO;
+    private StudentDao studentDao;
 
     public Set<Room> getAllRoom(){
         return roomDAO.getAll();
@@ -23,8 +27,8 @@ public class RoomService {
 
     public void addNewRoom() {
         Random r = new Random();
-        int number =  r.nextInt(100-100) + 10;
-        Room newRoom = new Room(number, HouseType.GRYFFINDOR);
+        int number =  r.nextInt(100-10) + 10;
+        Room newRoom = new Room(number, HouseType.GRYFFINDOR, null);
         roomDAO.add(newRoom);
     }
 
@@ -40,4 +44,20 @@ public class RoomService {
         Room updatedRoom = roomDAO.findById(roomID);
         updatedRoom.setHouseType(HouseType.SLYTHERIN);
     }
+
+
+    public void addStudentToEmptyRoom(Student currentStudent) {
+        Set<Room> allRoom = getAllRoom();
+        Set<Room> emptyRooms = new HashSet<>();
+        for (Room room : allRoom) {
+            if(room.getStudent() == null){
+                emptyRooms.add(room);
+            }
+
+        }
+        Room firstEmptyRoom = emptyRooms.stream().findFirst().orElse(null);
+        System.out.println(firstEmptyRoom);
+        roomDAO.updateEmptyRoomWithStudent(firstEmptyRoom, currentStudent);
+    }
+
 }
