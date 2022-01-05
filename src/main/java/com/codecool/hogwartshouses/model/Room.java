6d61ebe1af8503dbadfc0e4ec09e2d4e067d739a
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-05 11:02:00 +0100

    feat: add Student field to Room class.

--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -4,11 +4,15 @@ import com.codecool.hogwartshouses.model.types.HouseType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Room {
     private int id;
     private HouseType houseType;
+    private Student student;
+
 }
