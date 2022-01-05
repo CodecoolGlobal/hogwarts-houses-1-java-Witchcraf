43ddbb3b0565e53d049efb050c23bbf976aaf4b9
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-05 09:09:48 +0100

    feat: create studentCreator to init students.

---
+++ src/main/java/com/codecool/hogwartshouses/data_sample/StudentCreator.java
@@ -0,0 +1,28 @@
+package com.codecool.hogwartshouses.data_sample;
+
+import com.codecool.hogwartshouses.model.Student;
+import com.codecool.hogwartshouses.service.DAO.StudentMemory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentCreator {
+
+    private final StudentMemory studentMemory;
+
+    public void initialize() {
+        Student student = new Student("Alex");
+        Student student1 = new Student("Pete");
+        Student student2 = new Student("Mark");
+        studentMemory.add(student);
+        studentMemory.add(student1);
+        studentMemory.add(student2);
+    }
+
+    @Autowired
+    public StudentCreator(StudentMemory studentMemory) {
+        this.studentMemory = studentMemory;
+        initialize();
+    }
+
+}
