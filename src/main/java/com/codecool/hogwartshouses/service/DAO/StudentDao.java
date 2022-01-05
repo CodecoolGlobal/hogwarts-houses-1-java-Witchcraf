91ef947caf1e82d348431f830b76bb345b608f8d
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-05 09:10:16 +0100

    feat: create studentDao interface.

---
+++ src/main/java/com/codecool/hogwartshouses/service/DAO/StudentDao.java
@@ -0,0 +1,11 @@
+package com.codecool.hogwartshouses.service.DAO;
+
+import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
+
+import java.util.Set;
+
+public interface StudentDao {
+    void add(Student student);
+    Set<Student> getAll();
+}
