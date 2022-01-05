4d74c7abb221e3f85fb053c3291c634816a7ea67
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-05 11:08:48 +0100

    feat: refresh RoomCreator with student object.

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -1,32 +1,39 @@
 package com.codecool.hogwartshouses.data_sample;
 
 import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
 import com.codecool.hogwartshouses.model.types.HouseType;
 import com.codecool.hogwartshouses.service.DAO.RoomMemory;
+import com.codecool.hogwartshouses.service.DAO.StudentMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.UUID;
 
 @Component
 public class RoomCreator {
 
    private final RoomMemory roomMemory;
+   private final StudentMemory studentMemory;
 
     public void initialize() {
-        Room room = new Room(1, HouseType.GRYFFINDOR);
-        Room room1 = new Room(2, HouseType.HUFFLEPUFF);
-        Room room2 = new Room(3, HouseType.RAVENCLAW);
-        Room room3 = new Room(4, HouseType.SLYTHERIN);
-        for (Room room4 : Arrays.asList(room, room1, room2, room3)) {
-            roomMemory.add(room4);
-        }
+        Student st1 = new Student(123, "A");
+        Student st2 = new Student(124, "b");
+        Room room = new Room(1, HouseType.GRYFFINDOR, st1);
+        Room room1 = new Room(2, HouseType.HUFFLEPUFF, st2);
+        Room room2 = new Room(3, HouseType.RAVENCLAW, null);
+        Room room3 = new Room(4, HouseType.SLYTHERIN, null);
+        studentMemory.add(st1);
+        studentMemory.add(st2);
+        roomMemory.add(room);
+        roomMemory.add(room1);
+        roomMemory.add(room2);
+        roomMemory.add(room3);
     }
 
     @Autowired
-    public RoomCreator(RoomMemory roomMemory) {
+    public RoomCreator(RoomMemory roomMemory, StudentMemory studentMemory) {
         this.roomMemory = roomMemory;
+        this.studentMemory = studentMemory;
         initialize();
     }
 }
